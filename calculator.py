6da55d4327a2ad213eaa5dc2d15b9d060c1a0258
Sadat <sadatakhtar@hotmail.co.uk>
2019-01-30 21:57:36 +0000

    added a character just to check if my commit/push was succesfull

--- calculator.py
+++ calculator.py
@@ -1,1 +1,65 @@
-#Happy coding!!!!
+#Happy coding!!!!!!!!
+
+print('******************** Welcome to Calculator v1.1 ********************')
+print('********************     coded by shadowak47    ********************\n\n')
+
+cont = 'y'
+while cont == 'y':
+    selectOp =input('Select operation to perform(+,-,*,/):' )
+
+    if selectOp == '+' or selectOp == '-' or selectOp == '*' or selectOp == '/':
+        if selectOp == '+':
+            firstNum = input('Please enter the first number: ')
+            secNum = input('Please enter the second number: ')
+            print('The answer is ')
+            print(float(firstNum) + float(secNum))
+
+            cont = input('Perform another calculation(y/n)?')
+
+            if cont != 'y':
+                break
+
+
+        elif selectOp == '-':
+            firstNum = input('Please enter the first number: ')
+            secNum = input('Please enter the second number: ')
+            print('The answer is ')
+            print(float(firstNum) - float(secNum))
+
+            cont = input('Perform another calculation(y/n)?')
+
+            if cont != 'y':
+                break
+
+        elif selectOp == '*':
+            firstNum = input('Please enter the first number: ')
+            secNum = input('Please enter the second number: ')
+            print('The answer is ')
+            print(float(firstNum) * float(secNum))
+
+            cont = input('Perform another calculation(y/n)?')
+
+            if cont != 'y':
+                break
+
+        elif selectOp == '/':
+            firstNum = input('Please enter the first number: ')
+            secNum = input('Please enter the second number: ')
+            print('The answer is ')
+            print(float(firstNum) / float(secNum))
+
+            cont = input('Perform another calculation(y/n)?')
+
+            if cont != 'y':
+                break
+
+        else:
+            print('Incorrect operation!')
+
+    else:
+        print('Invalid selection!!!')
+        print('Only select from following options (+,-,*,/)')
+        cont = input('Continue(y/n)?')
+
+        if cont != 'y':
+            break
